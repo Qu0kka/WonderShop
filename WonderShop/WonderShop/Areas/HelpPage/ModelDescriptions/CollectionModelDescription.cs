5b8c7cbb917b716b607e90cc8097ffbf3e0cd2b9
Marina <marinka314@mail.ru>
2016-12-30 03:05:25 +0300

    Создание проекта

---
+++ WonderShop/WonderShop/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WonderShop.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
