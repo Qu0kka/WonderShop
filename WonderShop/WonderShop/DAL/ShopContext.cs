a2759c58d37e8f7406abe35de7b44ead3eb98a31
Marina <marinka314@mail.ru>
2017-01-04 18:12:44 +0300

    Создание новой папки DAL, создание класса ShopContext и свойства DbSet

---
+++ WonderShop/WonderShop/DAL/ShopContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using WonderShop.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace WonderShop.Models
+{
+    public class ShopContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
